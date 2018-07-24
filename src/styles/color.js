e0748134c6eeca38fe1e5360228a4674d73d7902
Richard Snider <richardnsnider@gmail.com>
2018-07-24 05:36:31 -0700

    Fix color values

--- src/styles/color.js
+++ src/styles/color.js
@@ -1,17 +1,7 @@
 export const color = {
-    black: {
-        color: '#000'
-    },
-    grey: {
-        color: '#888'
-    },
-    green: {
-        color: '#8f8'
-    },
-    blue: {
-        color: '#88f'
-    },
-    red: {
-        color: '#f88'
-    }
+    black: `#000`,
+    grey: `#888`,
+    green: `#8f8`,
+    blue: `#88f`,
+    red: `#f88`,
 }
